4d1d3c6afbd3e879dfad5f06e246e50a829051aa
EvertonFi <jose.everton.f@hotmail.com>
2020-02-20 11:08:48 -0300

    Update json_api_All.js

--- json_api_All.js
+++ json_api_All.js
@@ -59,26 +59,6 @@
       "idPrato": "7",
       "img": "",
       "idCategoria": "2"
-    },
-    {
-      "Nome_Categoria": "Bebidas",
-      "Acompanhamento": "Uma Coca-Cola Lata de 220ml",
-      "Descricao": "Sabe aquele sabor original de Coca-Cola bem gelada? Experimente.",
-      "Nome_Prato": "Coca-Cola Lata 220ml",
-      "Preco": "R$ 3,00",
-      "idPrato": "6",
-      "img": "",
-      "idCategoria": "2"
-    },
-    {
-      "Nome_Categoria": "Bebidas",
-      "Acompanhamento": "Um Guarana Antártica Lata de 220ml",
-      "Descricao": "Guaraná Antárctica é o refrigerante brasileiro com sabor único e natural.",
-      "Nome_Prato": "Guarana Antártica Lata 220ml",
-      "Preco": "R$ 3,00",
-      "idPrato": "7",
-      "img": "",
-      "idCategoria": "2"
     }
   ]
 }
